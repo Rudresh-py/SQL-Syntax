b8481c8a97e87fb7fe540f435924574bd41d6006
Rudreshcg <rudresha.gangaswamaiah@softsuave.com>
2023-07-05 19:43:00 +0530

    second commit and updated some new concepts

--- queries.sql
+++ queries.sql
@@ -98,3 +98,79 @@ SELECT *
 FROM (SELECT column_name(s) FROM table_name ORDER BY column_name(s))
 WHERE ROWNUM <= number;
 
+-- The SQL MIN() and MAX() Functions
+-- MIN() Syntax
+SELECT MIN(column_name)
+FROM table_name
+WHERE condition;
+
+-- MAX() Syntax
+SELECT MAX(column_name)
+FROM table_name
+WHERE condition;
+
+-- COUNT() Syntax
+SELECT COUNT(column_name)
+FROM table_name
+WHERE condition;
+The AVG() function returns the average value of a numeric column.
+
+-- AVG() Syntax
+SELECT AVG(column_name)
+FROM table_name
+WHERE condition;
+The SUM() function returns the total sum of a numeric column.
+
+-- SUM() Syntax
+SELECT SUM(column_name)
+FROM table_name
+WHERE condition;
+
+-- LIKE Syntax
+SELECT column1, column2, ...
+FROM table_name
+WHERE columnN LIKE pattern;
+
+
+-- LIKE operators with '%' and '_'
+LIKE Operator	                |     Description
+WHERE CustomerName LIKE 'a%'	| Finds any values that start with "a"
+WHERE CustomerName LIKE '%a'	| Finds any values that end with "a"
+WHERE CustomerName LIKE '%or%'	| Finds any values that have "or" in any position
+WHERE CustomerName LIKE '_r%'	| Finds any values that have "r" in the second position
+WHERE CustomerName LIKE 'a_%'	| Finds any values that start with "a" and are at least 2 characters in length
+WHERE CustomerName LIKE 'a__%'	| Finds any values that start with "a" and are at least 3 characters in length
+WHERE ContactName LIKE 'a%o'	| Finds any values that start with "a" and ends with "o"
+
+-- Wildcard Characters in SQL Server
+Symbol                    Description	                      Example
+%	           Represents zero or more characters	    bl% finds bl, black, blue, and blob
+_	           Represents a single character	        h_t finds hot, hat, and hit
+[]	           Represents any single character          within the brackets	h[oa]t finds hot and hat, but not hit
+^	           Represents any character not in          the brackets	h[^oa]t finds hit, but not hot and hat
+-	           Represents any single character          within the specified range	c[a-b]t finds cat and cbt
+
+
+-- LIKE operators with '%' and '_' wildcards:
+LIKE Operator	                |       Description
+WHERE CustomerName LIKE 'a%'	| Finds any values that starts with "a"
+WHERE CustomerName LIKE '%a'	| Finds any values that ends with "a"
+WHERE CustomerName LIKE '%or%'	| Finds any values that have "or" in any position
+WHERE CustomerName LIKE '_r%'	| Finds any values that have "r" in the second position
+WHERE CustomerName LIKE 'a__%'	| Finds any values that starts with "a" and are at least 3 characters in length
+WHERE ContactName LIKE 'a%o'	| Finds any values that starts with "a" and ends with "o"
+
+
+-- IN Syntax
+SELECT column_name(s)
+FROM table_name
+WHERE column_name IN (value1, value2, ...);
+-- or:
+SELECT column_name(s)
+FROM table_name
+WHERE column_name IN (SELECT STATEMENT);
+
+-- BETWEEN Syntax
+SELECT column_name(s)
+FROM table_name
+WHERE column_name BETWEEN value1 AND value2;
